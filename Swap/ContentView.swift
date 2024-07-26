c89759323e985541384683af446ddd65fb0c32cd
somaria <daryl.9@gmail.com>
2024-07-26 22:06:18 +0800

    update

--- Swap/ContentView.swift
+++ Swap/ContentView.swift
@@ -1,24 +1,49 @@
-//
-//  ContentView.swift
-//  Swap
-//
-//  Created by daryl on 26/7/24.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    let sentence = "This is a school."
+    let word = "school"
+    @State private var displayedSentence: String
+    @State private var isShowingCorrect = false
+
+    init() {
+        // Swap two characters in the word to create a misspelled version
+        var misspelledWord = word
+        let randomIndex = Int.random(in: 0..<(misspelledWord.count - 1))
+        let startIndex = misspelledWord.index(misspelledWord.startIndex, offsetBy: randomIndex)
+        let endIndex = misspelledWord.index(misspelledWord.startIndex, offsetBy: randomIndex + 1)
+        misspelledWord.swapAt(startIndex, endIndex)
+
+        // Replace the word in the sentence with the misspelled word
+        _displayedSentence = State(initialValue: sentence.replacingOccurrences(of: word, with: misspelledWord))
+    }
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text(isShowingCorrect ? sentence : displayedSentence)
+                .padding()
+                .font(.largeTitle)
+                .onTapGesture {
+                    isShowingCorrect.toggle()
+                }
+
+            if isShowingCorrect {
+                Text("Tap to see the incorrect version.")
+                    .padding()
+            } else {
+                Text("Tap to see the correct version.")
+                    .padding()
+            }
         }
-        .padding()
     }
 }
 
-#Preview {
-    ContentView()
+
+extension String {
+    mutating func swapAt(_ i: String.Index, _ j: String.Index) {
+        let temp = self[i]
+        self.replaceSubrange(i...i, with: [self[j]])
+        self.replaceSubrange(j...j, with: [temp])
+    }
 }
+
