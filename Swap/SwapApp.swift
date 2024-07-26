f8baa8ec85a9c82684732b3704ec10754393233d
somaria <daryl.9@gmail.com>
2024-07-26 21:41:33 +0800

    Initial Commit

---
+++ Swap/SwapApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwapApp.swift
+//  Swap
+//
+//  Created by daryl on 26/7/24.
+//
+
+import SwiftUI
+
+@main
+struct SwapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
